266e50716f5c43c923b276eefc3fc0a03860a469
Lazar Crina <crinalazar.a@gmail.com>
2019-01-06 18:35:07 +0200

    Third

---
+++ src/Shop/ItemsToBuy.java
@@ -0,0 +1,12 @@
+package Shop;
+
+public class ItemsToBuy {
+
+    int quantity;
+    Products product;
+
+    ItemsToBuy(Products product, int quantity){
+        this.quantity = quantity;
+        this.product = product;
+    }
+}
