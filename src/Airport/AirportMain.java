aeac24b6a50673d77810efaffb1001f3d8842d70
Lazar Crina <crinalazar.a@gmail.com>
2019-01-26 16:13:38 +0200

    Second

--- src/Airport/AirportMain.java
+++ src/Airport/AirportMain.java
@@ -4,9 +4,9 @@ package Airport;
 public class AirportMain {
     public static void main(String[] args) {
 
-        Flight f1 = new Flight( "FL-654", "Paris", "10/02/2019 20:30", 12000, 35 );
-        Flight f2 = new Flight( "FL-587", "Oslo", "22/01/2019 15:45", 27000, 40 );
-        Flight f3 = new Flight( "FL-325", "Frankfurt", "25/01/2019 21:30", 89540, 55 );
+        Flight f1 = new Flight( "FL-654", "Paris", "10/02/2019 20:30", 8500, 35 );
+        Flight f2 = new Flight( "FL-587", "Oslo", "22/01/2019 15:45", 9300, 40 );
+        Flight f3 = new Flight( "FL-325", "Frankfurt", "26/01/2019 14:15", 7200, 55 );
         User u1 = new User( "Maria", "Pop", "15.06.1990");
         User u2 = new User( "Marius", "Pop", "10.09.1985");
         User u3 = new User( "Ioana", "Tanase", "23.01.1977");
@@ -40,7 +40,7 @@ public class AirportMain {
         airportManager.getStatusFlights( StatusEnum.CANCELED );
         //airportManager.getStatusFlights(StatusEnum.PROCESSING);
 //       System.out.println(f2.getFlightStatus());
-//       System.out.println(f1.getFlightStatus());
+      ///System.out.println(f3.getFlightStatus());
 
 
 
